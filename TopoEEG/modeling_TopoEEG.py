4753284778819c7a6509a305f2a3202b5d3ea437
Eva Lebedyuk <114227265+le-beda@users.noreply.github.com>
2025-05-13 10:39:38 +0300

    Update modeling_TopoEEG.py

--- TopoEEG/modeling_TopoEEG.py
+++ TopoEEG/modeling_TopoEEG.py
@@ -13,12 +13,12 @@ class ModelArgs:
     reg1_order: int = 20
     topo1_n_long_living: int = 30
     classifier_base_n_out: int = 8
-    fusion_method: str = 'feature_level'
-    topo1_weight: float = 1/4
-    topo2_weight: float = 1/4
-    reg1_weight: float = 1/4
-    reg2_weight: float = 1/4
-    
+    fusion_method: str = "feature_level"
+    topo1_weight: float = 1 / 4
+    topo2_weight: float = 1 / 4
+    reg1_weight: float = 1 / 4
+    reg2_weight: float = 1 / 4
+
     def __init__(self, args):
         self.channels = args.channels
         self.all_classes = args.all_classes
@@ -33,12 +33,14 @@ class ModelArgs:
         self.reg1_weight = args.reg1_weight
         self.reg2_weight = args.reg2_weight
 
+
 class TopoEEG(nn.Module):
-    def __init__(self, 
-                 args,
-                 input_shapes, 
-                 classifier_base,
-                 ):
+    def __init__(
+        self,
+        args,
+        input_shapes,
+        classifier_base,
+    ):
         super(TopoEEG, self).__init__()
 
         self.fusion_method = args.fusion_method
@@ -49,18 +51,23 @@ class TopoEEG(nn.Module):
         self.input_shapes = input_shapes
         self.feature_extractors = nn.ModuleDict()
         self._build_feature_extractors()
-        
-        if self.fusion_method == 'feature_level':
+
+        if self.fusion_method == "feature_level":
             self._build_feature_level_classifier()
-        elif self.fusion_method == 'score_level':
+        elif self.fusion_method == "score_level":
             self._build_score_level_classifier()
-        elif self.fusion_method == 'decision_level':
+        elif self.fusion_method == "decision_level":
             self._build_decision_models()
-            self.weights = nn.Parameter(torch.Tensor([
-                args.topo1_weight,
-                args.topo2_weight,
-                args.reg1_weight,
-                args.reg2_weight]))
+            self.weights = nn.Parameter(
+                torch.Tensor(
+                    [
+                        args.topo1_weight,
+                        args.topo2_weight,
+                        args.reg1_weight,
+                        args.reg2_weight,
+                    ]
+                )
+            )
         else:
             raise ValueError(f"Unknown fusion method: {self.fusion_method}")
 
@@ -72,68 +79,47 @@ class TopoEEG(nn.Module):
             nn.Conv1d(32, 32, kernel_size=5, padding=2),
             nn.BatchNorm1d(32),
             nn.ReLU(),
-            nn.Flatten() 
-        )
-
-    def _build_CNN_2D(self, input_dim):
-        return nn.Sequential(
-            nn.Conv2d(input_dim[0], 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Flatten()
+            nn.Flatten(),
         )
 
     def _build_feature_extractors(self):
         for name, shape in self.input_shapes.items():
-            if len(shape) == 1:  # 1D features 
-                self.feature_extractors[name] = self._build_CNN_1D(shape)
-            elif len(shape) == 2:  # 2D features 
-                self.feature_extractors[name] = self._build_CNN_2D(shape)
-
+            self.feature_extractors[name] = self._build_CNN_1D(shape)
 
     def _build_feature_level_classifier(self):
         total_dim = 0
         for name, shape in self.input_shapes.items():
-            if len(shape) == 1:
-                total_dim += 32 * shape[0]  # 32 filters * n_features
-            elif len(shape) == 2:
-                total_dim += 32 * shape[1] * shape[2]  # 32 filters * n_nodes * n_nodes
+            total_dim += 32 * shape[0]  # 32 filters * n_features
 
-        self.final_classifier = nn.Sequential(self.classifier_base(total_dim, self.classifier_base_n_out),
-                                              nn.Linear(self.classifier_base_n_out, 1),
-                                              nn.Sigmoid()
-                                              )
+        self.final_classifier = nn.Sequential(
+            self.classifier_base(total_dim, self.classifier_base_n_out),
+            nn.Linear(self.classifier_base_n_out, 1),
+            nn.Sigmoid(),
+        )
 
     def _build_score_level_classifier(self):
         self.dense_layers = nn.ModuleDict()
 
         for name, shape in self.input_shapes.items():
-            if len(self.input_shapes[name]) == 1: 
-                input_dim = 32 * self.input_shapes[name][0]
-            elif len(shape) == 2:
-                input_dim = 32 * self.input_shapes[name][1] * self.input_shapes[name][2]
-            self.dense_layers[name] = self.classifier_base(input_dim, self.classifier_base_n_out)
-        
+            input_dim = 32 * shape[0]  # 32 filters * n_features
+            self.dense_layers[name] = self.classifier_base(
+                input_dim, self.classifier_base_n_out
+            )
+
         self.final_classifier = nn.Sequential(
             nn.Linear(self.classifier_base_n_out * len(self.input_shapes), 1),
-            nn.Sigmoid()
+            nn.Sigmoid(),
         )
 
     def _build_decision_models(self):
         self.decision_models = nn.ModuleDict()
         for name, shape in self.input_shapes.items():
-            if len(shape) == 1:  
-                input_dim = 32 * shape[0]
-            if len(shape) == 2: 
-                input_dim = 32 * shape[1] * shape[2]
-            
+            input_dim = 32 * shape[0]
+
             self.decision_models[name] = nn.Sequential(
                 self.classifier_base(input_dim, self.classifier_base_n_out),
                 nn.Linear(self.classifier_base_n_out, 1),
-                nn.Sigmoid()
+                nn.Sigmoid(),
             )
 
     def forward(self, x_dict):
@@ -141,23 +127,23 @@ class TopoEEG(nn.Module):
         features = {}
         for name in self.input_shapes.keys():
             x = x_dict[name]
-            if len(self.input_shapes[name]) == 1:  
-                x = x.unsqueeze(1)  
+            if len(self.input_shapes[name]) == 1:
+                x = x.unsqueeze(1)
             features[name] = self.feature_extractors[name](x)
-        
+
         # 1.
-        if self.fusion_method == 'feature_level':
+        if self.fusion_method == "feature_level":
             concatenated = torch.cat(list(features.values()), dim=1)
 
         # 2.
-        elif self.fusion_method == 'score_level':
+        elif self.fusion_method == "score_level":
             scores = []
             for name in self.input_shapes.keys():
                 scores.append(self.dense_layers[name](features[name]))
             concatenated = torch.cat(scores, dim=1)
-        
+
         # 3.
-        elif self.fusion_method == 'decision_level':
+        elif self.fusion_method == "decision_level":
             preds = []
             for name in self.input_shapes.keys():
                 preds.append(self.decision_models[name](features[name]))
@@ -166,4 +152,3 @@ class TopoEEG(nn.Module):
             return weighted_avg
 
         return self.final_classifier(concatenated)
-        
